0e24e2478a9895b51c075161e9285499d7f2dc76
Eloi Sanchez <eloisanchez16@gmail.com>
2023-06-22 15:36:38 +0200

    Made plot testing automatic. It gets all plots in the plots folder and tests them

--- tests/pytest/plot_making_test.py
+++ tests/pytest/plot_making_test.py
@@ -3,16 +3,31 @@ import sys
 
 import plotly
 
+import importlib
+
 # TODO: This is super weird for me. Is there a better solution?
 # Let me know at eloisanchez16@gmail.com
 sys.path.append(".")
 sys.path.append("./plots")
 
-from plots import schedule_by_year_month, duration_by_month
-
 
 TEST_DIR = os.path.dirname(__file__)
 PLOT_DIR = os.path.os.path.join(TEST_DIR, "../../plots/")
+
+# get name of all python plots in the plots directory
+modules = [
+    x.split(".")[0]
+    for x in filter(
+        lambda module: module != "utils.py"
+        and not os.path.isdir(os.path.join(PLOT_DIR, module)),
+        os.listdir(PLOT_DIR),
+    )
+]
+
+# import al python plots found
+plots = []
+for module in modules:
+    plots.append(importlib.import_module(module))
 
 
 def test_plot_dir() -> None:
@@ -20,6 +35,5 @@ def test_plot_dir() -> None:
 
 
 def test_plots() -> None:
-    plots = [duration_by_month, schedule_by_year_month]
     for plot in plots:
         assert type(plot.make_plot(testing=True)) == plotly.graph_objs.Figure
