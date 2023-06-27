cc209b2cd4ac9ce56eb4a8c8e22933f55c579d3b
Eloi Sanchez <eloisanchez16@gmail.com>
2023-06-27 10:24:17 +0200

    renamed folder with final tables

--- analyses/python/export_final_tables.py
+++ analyses/python/export_final_tables.py
@@ -5,7 +5,7 @@ import os
 
 def main():
     SCRIPT_DIR = os.path.dirname(__file__)
-    TABLES_DIR = os.path.join(SCRIPT_DIR, "../../models/final/sql/")
+    TABLES_DIR = os.path.join(SCRIPT_DIR, "../../models/main/sql/")
     EXPORT_DIR = os.path.join(SCRIPT_DIR, "../../data/final_tables/")
 
     tables = [x.split(".")[0] for x in os.listdir(TABLES_DIR)]
