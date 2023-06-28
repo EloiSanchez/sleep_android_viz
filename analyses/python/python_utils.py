8fcd64c6d2aef280725c4fe3580d46dc0f2466a7
Eloi Sanchez <eloisanchez16@gmail.com>
2023-06-28 16:22:03 +0200

    Created python script to run everything

--- analyses/python/python_utils.py
+++ analyses/python/python_utils.py
@@ -1,7 +1,9 @@
 import os
 
+
 SCRIPT_DIR = os.path.dirname(__file__)
-DB_DIR = os.path.os.path.join(SCRIPT_DIR, "../../../database/")
-FINAL_TABLES_DIR = os.path.join(SCRIPT_DIR, "../../models/main/sql/")
-FINAL_TABLES_EXPORT_DIR = os.path.join(SCRIPT_DIR, "../../data/final_tables/")
-PLOTS_DIR = os.path.join(SCRIPT_DIR, "../../plots/")
+PROJECT_DIR = os.path.abspath(os.path.join(SCRIPT_DIR, "../.."))
+DB_DIR = os.path.os.path.join(PROJECT_DIR, "database/")
+FINAL_TABLES_DIR = os.path.join(PROJECT_DIR, "models/main/sql/")
+FINAL_TABLES_EXPORT_DIR = os.path.join(PROJECT_DIR, "data/final_tables/")
+PLOTS_DIR = os.path.join(PROJECT_DIR, "plots/")
